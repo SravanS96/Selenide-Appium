9b5dfd70a1a6cf30a54e95b32964f678d9d54327
sravan <sravan@refyne.co.in>
2023-08-26 14:31:42 +0530

    Added selenide-appium properties file

---
+++ src/main/java/androidRunner/AndroidAppRunner.java
@@ -0,0 +1,31 @@
+package androidRunner;
+
+import com.codeborne.selenide.WebDriverProvider;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.remote.AutomationName;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import javax.annotation.Nonnull;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class AndroidAppRunner implements WebDriverProvider {
+    @Nonnull
+    @Override
+    public WebDriver createDriver(@Nonnull Capabilities capabilities) {
+
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setPlatformName("Android");
+        options.setDeviceName("My Testing");
+        options.setAutomationName(AutomationName.ANDROID_UIAUTOMATOR2);
+        options.setApp(System.getProperty("user.dir") + "/android-builds/app-uat-release.apk");
+        try {
+            return new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
