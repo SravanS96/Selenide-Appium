6d930cb83355436d8943a45a5812e51c48f9166f
sravan <sravan@refyne.co.in>
2023-08-26 14:42:33 +0530

    Code refactoring

--- src/test/java/android/splashScreen/AndroidSplashScreenTest.java
+++ src/test/java/android/splashScreen/AndroidSplashScreenTest.java
@@ -1,14 +1,20 @@
 package android.splashScreen;
 
 import com.codeborne.selenide.appium.SelenideAppium;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 
 public class AndroidSplashScreenTest {
 
+    @BeforeMethod
+    public void setUp(){SelenideAppium.launchApp();}
+
     @Test
     public void firstTest() throws InterruptedException {
-        SelenideAppium.launchApp();
+
         Thread.sleep(5000);
     }
+
+
 }
